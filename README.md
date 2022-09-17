e4d3faff3928e7c7b558786e7957ea94113745ec
jnosis <jnosis1546@gmail.com>
2022-09-17 15:59:04 +0900

    init Project

---
+++ README.md
@@ -0,0 +1,7 @@
+# unWebExtension
+
+Tool for building Web Extension with Deno.
+
+This tool id inspired by [bext](https://deno.land/x/bext) and [npm:@types/chrome](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/chrome).
+
+NOTE: Currently in developments for only [unMute](https://github.com/jnosis/unMute).
---
+++ src/mod.ts
@@ -0,0 +1,24 @@
+import type { BrowserAPI } from './types/browser_api.ts';
+import { isDeno, isFirefox } from 'bext/utilities/predicates.ts';
+import mockBrowser from './mock_browser/main.ts';
+
+export * from './types/browser_api.ts';
+export * from 'bext/utilities/predicates.ts';
+
+// deno-lint-ignore no-explicit-any
+let browserAPI: BrowserAPI = (globalThis as any).chrome;
+
+if (isFirefox()) {
+  // deno-lint-ignore no-explicit-any
+  browserAPI = (globalThis as any).browser;
+}
+
+// For running unit tests, use a mock browser structure
+// TODO: tree-shake this away in browser builds
+// TODO: Make this 1:1 BrowserAPI type
+if (isDeno()) {
+  // deno-lint-ignore no-explicit-any
+  browserAPI = mockBrowser as any;
+}
+
+export default browserAPI;
