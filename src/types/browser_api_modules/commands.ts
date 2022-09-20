61ca7154bef1c058adf357108dcdf0df773c6e2f
jnosis <jnosis1546@gmail.com>
2022-09-20 12:50:18 +0900

    Add commands api

--- src/types/browser_api_modules/commands.ts
+++ src/types/browser_api_modules/commands.ts
@@ -1,1 +1,26 @@
-export interface CommandsModule {}
+import { Event } from 'bext/types/browser_api_modules/event.ts';
+import { Tab } from './tabs.ts';
+
+export type Command = {
+  description?: string;
+  name?: string;
+  shortcut?: string;
+};
+
+export type CommandEvent = Event<(command: string, tab: Tab) => void>;
+
+export interface CommandsModule {
+  /**
+   * Returns all the registered extension commands for this extension and their shortcut (if active).
+   * @returns The 'getAll' method provides its result via callback or returned as a 'Promise' (MV3 only).
+   */
+  getAll(): Promise<Command[]>;
+  /**
+   * Returns all the registered extension commands for this extension and their shortcut (if active).
+   * @param callback
+   */
+  getAll(callback: (commands: Command[]) => void): void;
+
+  /** Fired when a registered command is activated using a keyboard shortcut */
+  onCommand: CommandEvent;
+}
