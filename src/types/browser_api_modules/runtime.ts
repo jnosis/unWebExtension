e6d00470687c081fbfb622eb64b4d8ddc542f716
jnosis <jnosis1546@gmail.com>
2022-09-22 09:58:28 +0900

    Update to add comments in runtime api

--- src/types/browser_api_modules/runtime.ts
+++ src/types/browser_api_modules/runtime.ts
@@ -4,8 +4,11 @@ import { Event } from 'bext/types/browser_api_modules/event.ts';
 import { Tab } from './tabs.ts';
 
 export type InstalledDetails = {
+  /** Optional. Indicates the ID of the imported shared module extension which updated. This is present only if 'reason' is 'shared_module_update'. */
   id?: string;
+  /** Optional. Indicates the previous version of the extension, which has just been updated. This is present only if 'reason' is 'update'. */
   previousVersion?: string;
+  /** The reason that this event is being dispatched. */
   reason: OnInstalledReason;
 };
 
@@ -25,22 +28,46 @@ export interface ManifestV3 extends ManifestBase {
 export type Manifest = ManifestV2 | ManifestV3;
 
 export type MessageOptions = {
-  /** Whether the TLS channel ID will be passed into onMessageExternal for processes that are listening for the connection event. */
+  /** Optional. Whether the TLS channel ID will be passed into onMessageExternal for processes that are listening for the connection event. */
   includeTlsChannelId?: boolean;
 };
 
+/**
+ * An object containing information about the script context that sent a message or request.
+ */
 export type MessageSender = {
+  /** Optional. A UUID of the document that opened the connection. */
   documentId?: string;
+  /** Optional. The lifecycle the document that opened the connection is in at the time the port was created. Note that the lifecycle state of the document may have changed since port creation. */
   documentLifeCycle?: string;
+  /** Optional. The frame that opened the connection. 0 for top-level frames, positive for child frames. This will only be set when tab is set. */
   frameId?: number;
+  /** Optional. The ID of the extension or app that opened the connection, if any. */
   id?: string;
+  /**
+   * Optional.
+   * Since Chrome 74.
+   * The name of the native application that opened the connection, if any.
+   */
   nativeApplication?: string;
+  /**
+   * Optional.
+   * Since Chrome 80.
+   * The origin of the page or frame that opened the connection. It can vary from the url property (e.g., about:blank) or can be opaque (e.g., sandboxed iframes). This is useful for identifying if the origin can be trusted if we can't immediately tell from the URL.
+   */
   origin?: string;
+  /** Optional. The tabs.Tab which opened the connection, if any. This property will only be present when the connection was opened from a tab (including content scripts), and only if the receiver is an extension, not an app. */
   tab?: Tab;
+  /** Optional. The TLS channel ID of the page or frame that opened the connection, if requested by the extension or app, and if available. */
   tlsChannelId?: string;
+  /** Optional. The URL of the page or frame that opened the connection. If the sender is in an iframe, it will be iframe's URL not the URL of the page which hosts it. */
   url?: string;
 };
 
+/**
+ * Since Chrome 44.
+ * The reason that this event is being dispatched.
+ */
 export type OnInstalledReason =
   | 'install'
   | 'update'
@@ -150,7 +177,12 @@ export interface RuntimeModule {
     callback?: (response: Response) => void,
   ): void;
 
+  /** Fired when the extension is first installed, when the extension is updated to a new version, and when Chrome is updated to a new version. */
   onInstalled: RuntimeInstalledEvent;
+
+  /** Fired when a message is sent from either an extension process (by runtime.sendMessage) or a content script (by tabs.sendMessage). */
   onMessage: ExtensionMessageEvent;
+
+  /** Fired when a profile that has this extension installed first starts up. This event is not fired when an incognito profile is started, even if this extension is operating in 'split' incognito mode. */
   onStartup: RuntimeEvent;
 }
