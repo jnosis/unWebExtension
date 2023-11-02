b3da2a3d1e4c7f27350b04b349890f2c28401660
jnosis <jnosis1546@gmail.com>
2023-11-02 12:54:43 +0900

    feat: Add notification template

--- src/template/ui.ts
+++ src/template/ui.ts
@@ -1,6 +1,17 @@
 const CONTEXT_MENUS = ``;
 
-const NOTIFICATION = ``;
+const NOTIFICATION = `import * as browser from '../api/api';
+
+export async function create() {
+  browser.notifications.create('updated', {
+    type: 'basic',
+    iconUrl: './icons/icon128.png',
+    title: browser.i18n.getMessage('notificationTitle'),
+    message: \`\${browser.i18n.getMessage('changelog_1_0_0')}\`,
+    // requireInteraction: true,
+  });
+}
+`;
 
 export const uiTemplate = {
   contextMenus: CONTEXT_MENUS,
