8f4f8f98dbe514571df1b98edf5477f65436f108
jnosis <jnosis1546@gmail.com>
2023-11-02 13:35:09 +0900

    feat: Convert create cmd into a sub to add others

--- src/command.ts
+++ src/command.ts
@@ -3,19 +3,24 @@ import { CreateWebExtension } from './create.ts';
 import { promptCreation } from './prompt.ts';
 import VERSION from './version.ts';
 
+const create = new Command()
+  .description('Create Web Extension project.')
+  .option('-b, --create-background', 'Create with background files.')
+  .option('-c, --create-content-scripts', 'Create with content scripts file.')
+  .option('-o, --create-options', 'Create with options files.')
+  .option('-p, --create-popup', 'Create with popup files.')
+  .option('-d, --directory=<directory:file>', 'Directory location.')
+  .arguments('[extensionName:string]')
+  .action(async (options, ...args) => {
+    await new CreateWebExtension(...await promptCreation(options, args[0]))
+      .create();
+  });
+
 export function command() {
   return new Command()
     .name('unWebExtension')
     .description('Tool for making Web Extension.')
     .version(VERSION)
-    .option('-b, --create-background', 'Create with background files.')
-    .option('-c, --create-content-scripts', 'Create with content scripts file.')
-    .option('-o, --create-options', 'Create with options files.')
-    .option('-p, --create-popup', 'Create with popup files.')
-    .option('-d, --directory=<directory>', 'Directory location.')
-    .arguments('[extensionName:string]')
-    .action(async (options, ...args) => {
-      await new CreateWebExtension(...await promptCreation(options, args[0]))
-        .create();
-    });
+    .help({ types: true })
+    .command('create', create);
 }
