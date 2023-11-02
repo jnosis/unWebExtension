7c64fa603cbac046b97ef6f62178075ca49d0620
jnosis <jnosis1546@gmail.com>
2023-11-02 13:09:44 +0900

    feat: Add context menus template

--- src/template/ui.ts
+++ src/template/ui.ts
@@ -1,4 +1,23 @@
-const CONTEXT_MENUS = ``;
+const CONTEXT_MENUS = `import * as browser from '../api/api';
+import { ContextMenuId } from '../types';
+
+export function createAll() {
+  console.trace('create all context menus');
+  browser.contextMenus.removeAll();
+
+  createById('shortcuts', true);
+  createById('changelog', true);
+}
+
+export function createById(id: ContextMenuId, isUI: boolean = false) {
+  console.trace(\`Create context menu: \${id}\`);
+  browser.contextMenus.create({
+    id,
+    title: browser.i18n.getMessage(\`contextMenu_\${id}\`),
+    contexts: isUI ? ['action'] : ['page', 'video', 'audio', 'action'],
+  });
+}
+`;
 
 const NOTIFICATION = `import * as browser from '../api/api';
 
