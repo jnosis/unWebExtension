8e26f8e2607fed91d383dd102cbbdb6c462bc6cb
jnosis <jnosis1546@gmail.com>
2024-04-27 15:11:11 +0900

    chore: Replace remote url imports by jsr

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,5 @@
-export type { CheckboxOption } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts';
-export { basename, resolve } from 'https://deno.land/std@0.196.0/path/mod.ts';
-export { colors } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/colors.ts';
-export { Command } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts';
-export {
-  Checkbox,
-  Confirm,
-  Input,
-} from 'https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts';
+export type { CheckboxOption } from '@cliffy/prompt';
+export { basename, resolve } from '@std/path';
+export { colors } from '@cliffy/ansi';
+export { Command } from '@cliffy/command';
+export { Checkbox, Confirm, Input } from '@cliffy/prompt';
